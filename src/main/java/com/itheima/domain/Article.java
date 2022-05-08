e6ce08f7b71272b8e66df1319aa47f390a3cd81e
stonebrave <situjiuxiang@163.com>
2022-05-08 20:06:11 +0800

    Initial commit

---
+++ src/main/java/com/itheima/domain/Article.java
@@ -0,0 +1,58 @@
+package com.itheima.domain;
+
+import java.util.List;
+
+/**
+ * @Classname Article
+ * @Description TODO
+ * @Date 2022/5/8 19:53
+ * @Created by situ
+ */
+public class Article {
+    private Integer id;
+    private String title;
+    private String content;
+    private List<Comment> commentList;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public List<Comment> getCommentList() {
+        return commentList;
+    }
+
+    public void setCommentList(List<Comment> commentList) {
+        this.commentList = commentList;
+    }
+
+    @Override
+    public String toString() {
+        return "Article{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                ", commentList=" + commentList +
+                '}';
+    }
+}
